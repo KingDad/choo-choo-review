3114f896bb35a4b0c6da3ad913fd4986a05fee55
Andrew Haveles <andxhav@gmail.com>
2019-04-22 15:45:57 -0400

    initial index work trains

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,2 +1,12 @@
 class TrainsController < ApplicationController
+
+  def index
+    @trains = Trains.all
+  end
+
+  def show
+    @train = Train.find(params[:id])
+    # @reviews = @train.reviews
+  end
+
 end
