c0e6a38ada1db5a11be24d859289253e1ecab7bb
KingDad <pgrcarlson@gmail.com>
2019-04-23 15:32:58 -0400

    Updated Train model test as per request

--- spec/models/train_spec.rb
+++ spec/models/train_spec.rb
@@ -8,34 +8,4 @@ describe Train do
   it { should_not have_valid(:description).when("", nil) }
   it { should have_valid(:founding_year).when('1990-01-01') }
   it {should_not have_valid(:founding_year).when('', nil)}
-
-  let(:train) {Train.create(name: 'Red Line', description: 'Its on fire!', founding_year: '1990-01-01')}
-  it 'will create a model for a train object' do
-    expect(train.name).to eq('Red Line')
-    expect(train.description).to eq('Its on fire!')
-    expect(train.founding_year).to eq(Date.parse('1990-01-01'))
-    expect(train).to be_a(Train)
-  end
-
-  let(:train2) {Train.create(description: 'Its on fire!', founding_year: '1990-01-01')}
-  it 'will create a model for a train object, and not validate due to lack of name' do
-    expect(Train.all.length).to eq(0)
-  end
-
-
-  let(:train3) {Train.create(name: 'redline', founding_year: '1990-01-01')}
-  it 'will create a model for a train object, and not validate due to lack of description' do
-    expect(Train.all.length).to eq(0)
-  end
-
-  let(:train4) {Train.create(name: 'redline', description: 'Its on fire!' )}
-  it 'will create a model for a train object, and not validate due to lack of date' do
-    expect(Train.all.length).to eq(0)
-  end
-
-  let(:train5) {Train.create()}
-  it 'will create a model for a train object, and not validate due to lack of name, date, and description' do
-    expect(Train.all.length).to eq(0)
-  end
-
 end
