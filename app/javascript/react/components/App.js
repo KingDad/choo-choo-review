5563b857aa545ac677c381f7e6eb2eeb5d6c025a
Keegan Rankin <Kms.rankin@gmail.com>
2019-04-23 16:55:55 -0400

    Connected react to rails train show, unfinished fetch

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import Train from './Train'
+
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <div>
+      <h1>Make It So React</h1>
+      <Train />
+    </div>
+  )
 }
 
 export default App
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+
   namespace :api do
     namespace :v1 do
       resources :trains, only: [:show]
     end
   end
-=======
+
   resources :trains, only: [:index, :show]
->>>>>>> b8bbdb21886a7454cccf77466b6888eac6f001e9
+
 end
