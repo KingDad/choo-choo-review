824a4d5378a4c425b813b3f6172ebf28b3b4c37c
KingDad <pgrcarlson@gmail.com>
2019-04-23 16:24:57 -0400

    Added user authentication for all train pages to require user to be admin except show and index

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,4 +1,5 @@
 class TrainsController < ApplicationController
+  before_action :authorize_user, except: [:index, :show]
 
   def index
     @trains = Train.all
@@ -6,6 +7,14 @@ class TrainsController < ApplicationController
 
   def show
     @train = Train.find(params[:id])
+  end
+
+  protected
+
+  def authorize_user
+    if !user_signed_in? || !current_user.admin?
+      flash[:notice] = "You do not have access to this page"
+    end
   end
 
 end
