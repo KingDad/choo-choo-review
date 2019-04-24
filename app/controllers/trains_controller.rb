e9214114bf05a6b9887330f8e1accf02f7b9b438
KingDad <pgrcarlson@gmail.com>
2019-04-24 11:13:07 -0400

    Added tests for submitting a new train successfully

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -35,7 +35,7 @@ class TrainsController < ApplicationController
 
   def authorize_user
     if !user_signed_in? || !current_user.admin?
-      flash[:notice] = "You do not have access to this page"
+      redirect_to '/users/sign_in'
     end
   end
 
--- spec/features/trains_new_spec.rb
+++ spec/features/trains_new_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 feature "Train new page" do
-  scenario "User visits new page" do
+  scenario "Admin visits new page" do
+    user = User.create(email: 'goodGuy@goodmail.com', password: 'iamsonice', password_confirmation: 'iamsonice', role: 'admin')
+    visit '/'
+    click_on 'Sign In'
+    fill_in 'Email', with: 'goodGuy@goodmail.com'
+    fill_in 'Password', with: 'iamsonice'
+    click_on 'Log in'
     visit '/trains/new'
     expect(page).to have_content("New Train Form")
     expect(page).to have_content("Name of Train")
     expect(page).to have_content("Description")
     expect(page).to have_content("Founding Date")
     expect(page).to have_selector("input", :count => 4)
+  end
+
+  scenario "Admin submits a train successfully" do
+    user = User.create(email: 'goodGuy@goodmail.com', password: 'iamsonice', password_confirmation: 'iamsonice', role: 'admin')
+    visit '/'
+    click_on 'Sign In'
+    fill_in 'Email', with: 'goodGuy@goodmail.com'
+    fill_in 'Password', with: 'iamsonice'
+    click_on 'Log in'
+    visit '/trains/new'
+    fill_in 'Name of Train', with: 'Fun Line'
+    fill_in 'Description', with: "It's a party up in here!"
+    fill_in 'Founding Date', with: "2019-11-11"
+    click_button 'Add Train'
+    expect(page).to have_content('Fun Line')
   end
 end
